7061b0e3ea36702f48a9029f11e7891a5d86de59
angedev07 <angelgaran20@gmail.com>
2023-11-23 19:00:47 -0500

    fix(admin layout): Fixed some bugs in admin layout.

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -1,17 +1,16 @@
-import {AppBar, Box, Toolbar} from "@mui/material";
-import {useState} from "react";
-import {Navigate, Outlet} from "react-router-dom";
-import {Navbar} from "./components/Navbar";
-import {Sidebar} from "./components/Sidebar";
-import {useAppSelector} from "../store";
+import { AppBar, Box, Toolbar } from "@mui/material";
+import { useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Sidebar } from "./components/Sidebar";
+import { useAppSelector } from "../store";
 
 export const AdminLayout = () => {
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
   const [mobileOpen, setMobileOpen] = useState(true);
-  const user = useAppSelector(state =>  state.authentication.user)
+  const user = useAppSelector((state) => state.authentication.user);
 
-    if (!user)
-        return <Navigate to="/auth" />;
+  if (!user) return <Navigate to="/auth" />;
 
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
@@ -54,7 +53,21 @@ export const AdminLayout = () => {
         user={user!}
       />
 
-      <Box component="main" flexGrow={1} p={2}  sx={{ marginLeft: { md : mobileOpen ? '268px' :  ' 0', transition: 'margin 0.2s ease-in-out', width: 'calc(100% - 268px)'} }}>
+      {/* main content */}
+      <Box
+        component="main"
+        flexGrow={1}
+        p={2}
+        sx={{
+          marginLeft: {
+            md: mobileOpen ? "268px" : " 0",
+            transition: "margin 0.2s ease-in-out",
+            width: { md: "calc(100% - 268px)", xs: "100%"},
+            height: "calc(100vh - 64px)",
+            overflowY: "scroll",
+          },
+        }}
+      >
         <Outlet />
       </Box>
     </Box>
